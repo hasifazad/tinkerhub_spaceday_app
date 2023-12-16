00f77ad75e34e01be1bbae836c78a98517de3299
wajid <abdulwajidnv@gmail.com>
2023-12-16 18:57:22 +0530

    Add styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,32 @@
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
 
 function Header() {
-    return (
-        <>
-            <Grid container borderBottom={1} justifyContent='space-between' paddingX={6} paddingY={2}>
-                <Grid item >
-                    <Typography variant='h5' fontWeight={600}>TinkerSpace Carnival</Typography>
-                </Grid>
-                <Grid item>
-                    <Typography variant='h5' fontWeight={600}>Tinker
-                        <span style={{ fontWeight: 'lighter' }}>
-                            Hub
-                        </span>
-                    </Typography>
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <Grid
+        container
+        borderBottom={1}
+        justifyContent="space-between"
+        paddingX={6}
+        paddingY={2}
+      >
+        <Grid item>
+          <Typography variant="h5" fontFamily="InterBold">
+            TinkerSpace Carnival
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" fontFamily="InterBold" display="flex">
+            Tinker
+            <Typography variant="h5" fontFamily="InterLight">
+              Hub
+            </Typography>
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default Header
+export default Header;
